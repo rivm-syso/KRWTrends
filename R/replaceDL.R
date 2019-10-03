26bd96671b3ec0dd8ac0b6ba1354eb57500dd21d
witm <witm@rl04-int-p.rivm.nl>
2019-10-03 12:54:31 +0200

    Functie om rapportagegrenzen aan te passen voor de trendanalyse op reeks niveau

---
+++ R/replaceDL.R
@@ -0,0 +1,43 @@
+#' Vervang waardes onder de detectielimiet. 
+#' 
+#' Help functie voor \code{lmgsubset} om waardes onder de 
+#' detectielimiet te vervangen.
+#' 
+#' Deze functie vervangt waardes onder de detectielimiet. Deze 
+#' waardes zijn gecodeerd als in de kolom detectielimiet door 1.
+#' Om de detectielimet te vervangen voor de helft van de
+#' oorspronkelijke waarde moet de vermenigvuldigingsfactor 0,5 zijn.
+#' 
+#' @param x vector met waardes
+#' @param replaceval vermenigvuldigingsfactor om rapportagegrens waardes
+#' te vervangen
+#' @return een vector met waardes waar de detectielimiet is vervangen
+#' 
+#' @examples
+#' data(lmg_reeks)
+#' d <- replaceDL(lmg_reeks, 0.5)
+#'
+#' @export
+
+replaceNegative <- function(d, replaceval = 0.5) {
+  
+  # Check of er maar één reeks wordt ingevoerd om de DG aan te passen
+  if(unique(d$putfilter) > 1){
+    stop("de reeks bevat meerdere putfilters,
+         maar mag er maar 1 bevatten")
+  }
+
+  if(unique(d$parameter) > 1){
+    stop("de reeks bevat meerdere parameters, 
+         maar mag er maar 1 bevatten")
+  }
+  
+  laagste_waarneming <- d$waarde[d$detectielimiet == 0] %>% min(na.rm = TRUE) 
+  hoogste_RG_onder_waarneming <- d$waarde[d$detectielimiet == 1 & d$waarde < laagste_waarneming] %>% max(na.rm = TRUE)
+  
+  d <- d %>% mutate(waarde = ifelse(waarde < laagste_waarneming & detectielimiet == 1, replaceval * hoogste_RG_onder_waarneming, waarde))
+
+  return(d)
+}
+
+
