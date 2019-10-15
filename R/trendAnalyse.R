b862ad44bfb97c49e73dee3fe97825f3c8530b16
witm <witm@rl02-int-p.rivm.nl>
2019-10-15 11:56:41 +0200

    Functie moet/kan alleen lopen als meer dan 0 regels, anders NA returnen. Leesbaarheid verbeterd.

--- R/trendAnalyse.R
+++ R/trendAnalyse.R
@@ -19,9 +19,11 @@
 
 trendAnalyse <- function(d) {
 
+if(nrow(d) > 0) {  
+  
 i <- as.character(unique(d$putfilter))
-res2  <- lapply(i,FUN=mktrends,d) %>%
-    do.call("rbind",args=.) %>%
+res2 <- lapply(i, FUN = mktrends, d) %>%
+    do.call("rbind", args =.) %>%
     na.omit()
 
 if(nrow(res2) > 0) {
@@ -31,47 +33,48 @@ n.total <- nrow(res2)
 
 # samenvatting benoemde trends en richting
 d.trend1 <- res2 %>%
-    filter(trend=="trend",p<=fdr$threshold) %>%
-    mutate(trend="trend- benoemd") %>%
-    group_by(trend,direction) %>%
-    summarise(n=n(),
-              percentage=n()/n.total,
-              gemiddeld.helling=mean((slope))) %>%
+    filter(trend == "trend", p <= fdr$threshold) %>%
+    mutate(trend = "trend- benoemd") %>%
+    group_by(trend, direction) %>%
+    summarise(n = n(),
+              percentage = n() / n.total,
+              gemiddeld.helling = mean((slope))) %>%
     ungroup
 
 # samenvatting benoemde trends, totaal op en neerwaartse richting
 d.trend2  <- res2 %>% 
-    filter(trend=="trend",p<=fdr$threshold) %>%
-    mutate(trend="trend- benoemd") %>%
+    filter(trend == "trend", p <= fdr$threshold) %>%
+    mutate(trend = "trend- benoemd") %>%
     group_by(trend) %>%
-    summarise(n=n(),
-              percentage=n()/n.total,
-              gemiddeld.helling=mean((slope))) %>%
+    summarise(n = n(),
+              percentage = n() / n.total,
+              gemiddeld.helling = mean((slope))) %>%
     ungroup 
 
 
 # samenvatting niet benoemde trends (p>fdr threshold)
 d.trend3  <- res2 %>% 
-    filter(trend=="trend") %>%
-    mutate(trend="trend- niet benoemd") %>%
+    filter(trend == "trend") %>%
+    mutate(trend = "trend- niet benoemd") %>%
     group_by(trend) %>%
-    summarise(n=n(),
-              percentage=n()/n.total,
-              gemiddeld.helling=mean((slope))) %>%
+    summarise(n = n(),
+              percentage = n() / n.total,
+              gemiddeld.helling = mean((slope))) %>%
     ungroup 
 
-d.trend <- bind_rows(d.trend1,d.trend2,d.trend3) %>%
-    mutate(direction=ifelse(is.na(direction),"op/neer",direction)) %>%
-    mutate(statistiek=paste(trend,direction)) %>%
-    select(statistiek,n,percentage,gemiddeld.helling)
+d.trend <- bind_rows(d.trend1, d.trend2, d.trend3) %>%
+    mutate(direction = ifelse(is.na(direction), "op/neer" ,direction)) %>%
+    mutate(statistiek = paste(trend, direction)) %>%
+    select(statistiek, n, percentage, gemiddeld.helling)
 
+}
 } else {
   d.trend <- NA
 }
 
 return(d.trend)
 
-}
 
+}
 
 
