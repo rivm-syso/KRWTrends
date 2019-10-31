8a37eb8c1730606188c02f9d607b8ecb273341d1
witm <witm@rl02-int-p.rivm.nl>
2019-10-31 11:13:17 +0100

    Waarde is waarde, zodat deze compatibel is met de dataset

--- R/applyTheilSen.R
+++ R/applyTheilSen.R
@@ -7,19 +7,19 @@ apply.Theil.Sen <- function(y,Series){
 
     aux.Series <- subset(Series,Jaar<=y)
 
-    Theil.Sen <- mblm::mblm(Waarde~Jaar,dataframe=aux.Series,repeated=FALSE)$coefficients
+    Theil.Sen <- mblm::mblm(waarde~Jaar,dataframe=aux.Series,repeated=FALSE)$coefficients
     slope.1 <- Theil.Sen[2]
     intercept.1 <- Theil.Sen[1]
     aux.Series <- transform(aux.Series,residual=NA)
-    aux.Series$residual <- aux.Series$Waarde-(intercept.1+slope.1*aux.Series$Jaar)
+    aux.Series$residual <- aux.Series$waarde-(intercept.1+slope.1*aux.Series$Jaar)
     RSS.1 <- sum(aux.Series$residual^2)
 
     aux.Series <- subset(Series,Jaar>=y)
-    Theil.Sen <- mblm::mblm(Waarde~Jaar,dataframe=aux.Series,repeated=FALSE)$coefficients
+    Theil.Sen <- mblm::mblm(waarde~Jaar,dataframe=aux.Series,repeated=FALSE)$coefficients
     slope.2 <- Theil.Sen[2]
     intercept.2 <- Theil.Sen[1]
     aux.Series <- transform(aux.Series,residual=NA)
-    aux.Series$residual <- aux.Series$Waarde-(intercept.2+slope.2*aux.Series$Jaar)
+    aux.Series$residual <- aux.Series$waarde-(intercept.2+slope.2*aux.Series$Jaar)
     RSS.2 <- sum(aux.Series$residual^2)
 
     RSS <- RSS.1+RSS.2
