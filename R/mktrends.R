853170e92e6ec28043ee68d9af85e63bf8351663
witm <witm@rl02-int-p.rivm.nl>
2019-11-20 12:32:04 +0100

    Haakje te veel

--- R/mktrends.R
+++ R/mktrends.R
@@ -91,7 +91,7 @@ mktrends <- function(i, x , dw, trim = FALSE, trimfactor = 1.5,
         p <- p + theme(legend.position = "none", 
                        axis.text.x = element_text(angle = 90, hjust = 1))
         p <- p + scale_x_continuous(breaks = d$jr, labels = d$meetjaar)
-        p <- p + labs(x = "Jaar", y = paste("Concentratie", param, strsplit(x$eenheid[1], " ")[[1]][1])),
+        p <- p + labs(x = "Jaar", y = paste("Concentratie", param, strsplit(x$eenheid[1], " ")[[1]][1]),
                       title = paste("Trend in filter ", i))
         p <- p + theme(plot.title = element_text(hjust = 0.5),
                        legend.position = "bottom")
