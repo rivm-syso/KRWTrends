532ffbdbd546f38f6ec05f481b94221352fceaa7
Lisette Avis <lisette.avis@rhdhv.com>
2020-03-09 11:09:19 +0100

    Accolade regel 79 verplaatst naar 79 in nieuwe code (stond verkeerd, moest na de else-statement). Overbodige enters weggehaald en if-statements netter uitgelijnd met tabs.

--- R/trendAnalyse.R
+++ R/trendAnalyse.R
@@ -25,65 +25,59 @@ trendAnalyse <- function(d, n.reeks) {
     return("totaal aantal reeksen onbekend om % trends te berekenen")
   }
   
-if(nrow(d) > 0) {  
-  
-i <- as.character(unique(d$putfilter))
-res2 <- lapply(i, FUN = mktrends, d) %>%
-    do.call("rbind", args =.) %>%
-    na.omit()
+  if(nrow(d) > 0) {  
+    i <- as.character(unique(d$putfilter))
+    res2 <- lapply(i, FUN = mktrends, d) %>%
+        do.call("rbind", args =.) %>%
+        na.omit()
+    
+    if(nrow(res2) > 0) {
+      fdr <- res2 %>% select(p) %>% bhfdr()
+      n.mk <- nrow(res2)
+      
+      # samenvatting benoemde trends en richting
+      d.trend1 <- res2 %>%
+          filter(trend == "trend", p <= fdr$threshold) %>%
+          mutate(trend = "trend- benoemd") %>%
+          group_by(trend, direction) %>%
+          summarise(n = n(),
+                    percentage = paste(round(n() / n.reeks, digits = 2), round(n() / n.mk, digits = 2), sep = " / " ),
+                    gemiddeld.helling = mean((slope)),
+                    putfilters = paste(sort(unique(putfilter)), collapse = ", ")) %>%
+          ungroup
+      
+      # samenvatting benoemde trends, totaal op en neerwaartse richting
+      d.trend2  <- res2 %>% 
+          filter(trend == "trend", p <= fdr$threshold) %>%
+          mutate(trend = "trend- benoemd") %>%
+          group_by(trend) %>%
+          summarise(n = n(),
+                    percentage = paste(round(n() / n.reeks, digits = 2), round(n() / n.mk, digits = 2), sep = " / " ),
+                    gemiddeld.helling = mean((slope)),
+                    putfilters = paste(sort(unique(putfilter)), collapse = ", ")) %>%
+          ungroup 
 
-if(nrow(res2) > 0) {
-
-fdr <- res2 %>% select(p) %>% bhfdr()
-n.mk <- nrow(res2)
-
-# samenvatting benoemde trends en richting
-d.trend1 <- res2 %>%
-    filter(trend == "trend", p <= fdr$threshold) %>%
-    mutate(trend = "trend- benoemd") %>%
-    group_by(trend, direction) %>%
-    summarise(n = n(),
-              percentage = paste(round(n() / n.reeks, digits = 2), round(n() / n.mk, digits = 2), sep = " / " ),
-              gemiddeld.helling = mean((slope)),
-              putfilters = paste(sort(unique(putfilter)), collapse = ", ")) %>%
-    ungroup
-
-# samenvatting benoemde trends, totaal op en neerwaartse richting
-d.trend2  <- res2 %>% 
-    filter(trend == "trend", p <= fdr$threshold) %>%
-    mutate(trend = "trend- benoemd") %>%
-    group_by(trend) %>%
-    summarise(n = n(),
-              percentage = paste(round(n() / n.reeks, digits = 2), round(n() / n.mk, digits = 2), sep = " / " ),
-              gemiddeld.helling = mean((slope)),
-              putfilters = paste(sort(unique(putfilter)), collapse = ", ")) %>%
-    ungroup 
-
-
-# samenvatting niet benoemde trends (p>fdr threshold)
-d.trend3  <- res2 %>% 
-    filter(trend == "trend") %>%
-    mutate(trend = "trend- niet benoemd") %>%
-    group_by(trend) %>%
-    summarise(n = n(),
-              percentage = paste(round(n() / n.reeks, digits = 2), round(n() / n.mk, digits = 2), sep = " / " ),
-              gemiddeld.helling = mean((slope)),
-              putfilters = paste(sort(unique(putfilter)), collapse = ", ")) %>%
-    ungroup 
-
-d.trend <- bind_rows(d.trend1, d.trend2, d.trend3) %>%
-    mutate(direction = ifelse(is.na(direction), "op/neer" ,direction)) %>%
-    mutate(statistiek = paste(trend, direction)) %>%
-    select(statistiek, n, percentage, gemiddeld.helling, putfilters)
-
-}
-} else {
-  d.trend <- NA
-}
-
-return(d.trend)
-
-
+      # samenvatting niet benoemde trends (p>fdr threshold)
+      d.trend3  <- res2 %>% 
+          filter(trend == "trend") %>%
+          mutate(trend = "trend- niet benoemd") %>%
+          group_by(trend) %>%
+          summarise(n = n(),
+                    percentage = paste(round(n() / n.reeks, digits = 2), round(n() / n.mk, digits = 2), sep = " / " ),
+                    gemiddeld.helling = mean((slope)),
+                    putfilters = paste(sort(unique(putfilter)), collapse = ", ")) %>%
+          ungroup 
+      
+      d.trend <- bind_rows(d.trend1, d.trend2, d.trend3) %>%
+          mutate(direction = ifelse(is.na(direction), "op/neer" ,direction)) %>%
+          mutate(statistiek = paste(trend, direction)) %>%
+          select(statistiek, n, percentage, gemiddeld.helling, putfilters)
+      
+      } else {
+        d.trend <- NA
+      }
+  }
+  return(d.trend)
 }
 
 
